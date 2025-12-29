ff6ae01e1199654c20287be7d892ad62a723af7c
Pablo Gaeta <pablogaeta@google.com>
2025-12-28 19:48:46 -0600

    add fish config

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,7 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+source "$HOME/.cargo/env.fish"
+
+alias ep="vim $HOME/.config/fish/config.fish"
+alias ep="source $HOME/.config/fish/config.fish"
+alias hypr="vim $HOME/.config/hypr/hyprland.conf"
