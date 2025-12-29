9b378302081be08ca642664215f16cf28675863f
Pablo Gaeta <pablofgaeta@gmail.com>
2025-12-29 09:16:28 -0600

    fix paths

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,6 +2,8 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 
 source "$HOME/.cargo/env.fish"
 
+alias vim=nvim
+
 alias ep="vim $HOME/.config/fish/config.fish"
 alias ep="source $HOME/.config/fish/config.fish"
 alias hypr="vim $HOME/.config/hypr/hyprland.conf"
